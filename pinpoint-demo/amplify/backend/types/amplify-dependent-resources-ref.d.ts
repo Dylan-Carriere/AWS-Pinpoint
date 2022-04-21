b724c8a30d416f37f16a2a0048187dfdec162b1c
Dylan-Carriere <dylan31120@gmail.com>
2022-04-21 23:47:00 +0200

    finish tutorial on amplify

--- pinpoint-demo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ pinpoint-demo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "pinpointdemo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "pinpointdemo09994e69": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
